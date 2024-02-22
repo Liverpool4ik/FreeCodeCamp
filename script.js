a440a93345e486dc98d13fcf3187e6f5fe9483cb
Liverpool4ik <liverpool4ik@proton.me>
2024-02-22 16:12:27 +0200

    JS-Cave-Dragon - Step 42

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth ");
+
 function goStore() {
-	console.log("Going to store.");
+	button1.innerText = "Buy 10 health (10 gold)";
 }
 
 function goCave() {
