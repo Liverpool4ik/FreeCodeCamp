ee454bef7d66ec02132685938d090cdcbeb18c76
Liverpool4ik <liverpool4ik@proton.me>
2024-02-29 23:32:18 +0200

    JS-Cave-Dragon - Step 54

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth ");
+const locations = [
+	{
+		name: "town square",
+		"button text": [],
+	},
+];
 
 function goTown() {
 	button1.innerText = "Go to store";
